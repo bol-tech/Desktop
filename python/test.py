afaa8dd9d3f929f75a590d41152b292521432e29
Bol Isaac <mbal48627@gmail.com>
2021-11-09 00:02:41 +0530

    st

--- python/test.py
+++ python/test.py
@@ -10,6 +10,13 @@ qualification = input("Degree, Diploma,master or PHD.........")
 profession = input("specify your IT expertise_______")
 DOB  = input("DDD/MM/YYY  //....//....///")
 print("PA-background:----",qualification,profession,DOB)
+marital_status = "married"
+if marital_status == 'married':
+    print("please confirm your status")
+    name = input("his/her name is.... ..........")
+    
+    
+    
 
 
 
